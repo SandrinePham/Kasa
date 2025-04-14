e16ad2d92c222ab851691c181b9d18b0e99522a0
Sandrine P <sandrine.pham.89@gmail.com>
2025-04-14 16:46:56 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import AppRouter from './routes/AppRouter';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return <AppRouter />;
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import "./NavBar.scss";
+
+function Navbar() {
+  return (
+    <nav>
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+        end
+      >
+        Accueil
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) => (isActive ? 'active' : '')}
+      >
+        À Propos
+      </NavLink>
+    </nav>
+  );
+}
+
+export default Navbar;
